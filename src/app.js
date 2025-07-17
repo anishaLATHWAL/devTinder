5e721fc84e09d5a75efa685f21dcbb8807fd4d30
anishaLATHWAL <anishalathwalal1983@gmail.com>
2025-07-17 16:24:04 +0530

    Creating express server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.use("/",(req,res)=>{
+    res.send("Hello from the dashboard");
+})
+app.use("/hello",(req,res)=>{
+    res.send("Hello hello hello");
+})
+// app.use((req,res)=>{
+//     res.send("Hello from the server");
+// })
+
+app.listen(3000,()=>{
+    console.log("Servcer is successfully running on port 3000....");
+})
