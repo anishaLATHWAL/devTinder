501606de070bf43f53eafac72b53095046125fa0
anishaLATHWAL <anishalathwalal1983@gmail.com>
2025-07-17 19:29:15 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
-const express = require('express');
+const express = require('express'); // importing express
 
-const app = express();
+const app = express(); // returns an application object app
 
-app.use("/",(req,res)=>{
-    res.send("Hello from the dashboard");
+// Route handlers
+
+app.use("/test",(req,res)=>{
+    res.send("Hello from the server");
 })
-app.use("/hello",(req,res)=>{
-    res.send("Hello hello hello");
+
+app.get("/user",(req,res)=>{
+    res.send({ firstname : "Anisha", lastName : "Lathwal" })
 })
-// app.use((req,res)=>{
-//     res.send("Hello from the server");
+app.post("/user",(req,res)=>{
+    // save data to the database
+    res.send("Data successfully saved to the database")
+})
+app.delete("/user",(req,res)=>{
+    // save data to the database
+    res.send("Deleted successfully")
+})
+
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello hello hello");
+// })
+
+// app.use("/",(req,res)=>{
+//     res.send("Hello from the dashboard");
 // })
 
 app.listen(3000,()=>{
